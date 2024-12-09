20a7732314e5be3478cfb5b79dbe1858ff2a4c3e
Franklyn Vasquez <franklynvasquez24@gmail.com>
2024-12-09 22:42:50 +0200

    Added default constructor for loading

--- PanelControllerBasics/LogOut.cs
+++ PanelControllerBasics/LogOut.cs
@@ -18,6 +18,8 @@ namespace PanelControllerBasics
         [ItemName]
         public string Name { get => $"LogOut ({Sender}) {Message}"; }
 
+        public LogOut() { }
+
         [UserConstructor("Create an action that when ran, logs out a message to the PanelController Logger.")]
         public LogOut(Logger.Levels level,  string sender, string message)
         {
